7736e2b6883190095703e854c3002de9c5db7182
Guilherme Ramos Mendes <guilherme.rmendes95@gmai.com>
2021-05-16 23:21:09 -0300

    Editing files

--- src/app/photo-gallery/photo-gallery.page.spec.ts
+++ src/app/photo-gallery/photo-gallery.page.spec.ts
@@ -1,3 +1,14 @@
-describe('PhotoGalleryPage', () => {
+import { ActionSheetController } from "@ionic/angular";
+import { PhotoService } from "../services/photo.service";
+import { PhotoGalleryPage } from "./photo-gallery.page";
 
+describe('PhotoGalleryPage', () => {
+    let component: PhotoGalleryPage;
+    let photoService: PhotoService;
+    let actionSheetController: ActionSheetController
+
+    it('Should create component', () => {
+        component = new PhotoGalleryPage(photoService, actionSheetController);
+        expect(component).toBeTruthy();
+    })
 });
